e11ebfc917666013886a12076215350bcc53b91d
pushpqa <pushpqa1@gmail.com>
2025-02-02 15:52:56 -0600

    Fixed

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 test.describe('group-1', () => {
-test.beforeAll(async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://www.google.com/');
 });
-test.afterAll(async ({ page }) => { await page.close(); });
+test.afterEach(async ({ page }) => { await page.close(); });
 
 test('test-1', async ({ page }) => {
   console.log('test-1');
-  await page.goto('https://www.google.com/');
+  // await page.goto('https://www.google.com/');
   await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
   await page.getByRole('combobox', { name: 'Search' }).click();
   
