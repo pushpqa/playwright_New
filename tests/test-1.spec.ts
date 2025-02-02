dbc0866224c4c9e857e9407935a45571095b9273
pushpqa <pushpqa1@gmail.com>
2025-02-02 15:43:58 -0600

    Added before all method

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from '@playwright/test';
+test.describe('group-1', () => {
+test.beforeAll(async ({ page }) => {
+  await page.goto('https://www.google.com/');
+});
+test.afterAll(async ({ page }) => { await page.close(); });
 
 test('test-1', async ({ page }) => {
   console.log('test-1');
   await page.goto('https://www.google.com/');
   await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
   await page.getByRole('combobox', { name: 'Search' }).click();
+  
 });
 
 test('test-2', async ({ page }) => {
@@ -12,4 +18,12 @@ test('test-2', async ({ page }) => {
   await page.goto('https://www.google.com/');
   await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
   await page.getByRole('combobox', { name: 'Search' }).click();
+});
+
+test('test-3', async ({ page }) => {
+  console.log('test-3');
+  await page.goto('https://www.google.com/');
+  await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
+  await page.getByRole('combobox', { name: 'Search' }).click();
+});
 });
