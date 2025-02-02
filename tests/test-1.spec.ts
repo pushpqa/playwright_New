f3b5871a0ab4e5237a11e3cd8cdae4c34e18d34b
pushpqa <pushpqa1@gmail.com>
2025-02-02 15:10:46 -0600

    Added sample test suit

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
- 
+test('test-1', async ({ page }) => {
+  console.log('test-1');
   await page.goto('https://www.google.com/');
   await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
   await page.getByRole('combobox', { name: 'Search' }).click();
-  });
+});
+
+test('test-2', async ({ page }) => {
+  console.log('test-2');
+  await page.goto('https://www.google.com/');
+  await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
+  await page.getByRole('combobox', { name: 'Search' }).click();
+});
